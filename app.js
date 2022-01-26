140722cff5a3c02e08a13c5acce5954785720db6
Tom <87855471+Tom2612@users.noreply.github.com>
2022-01-26 18:42:36 +0800

    Update app.js
    Added full game control. Begun adding restart

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Player = (name, token) => {
 };
 
 const gameBoard = (() => {
-    let board = ["X","X","X","X","X","X","X","X","X"];
+    let board = ["","","","","","","","",""];
     const boardDisplay = document.querySelector('.boardDisplay');
     const boxList = boardDisplay.childNodes;
 
@@ -18,27 +18,29 @@ const gameBoard = (() => {
        };
     };
 
-    const setSquare = (index, token) => {
-        if (index > board.length) return;
-        board[index] = token;
-    };
-
-    const getField = (index) => {
-        if (index > board.length) return;
-        return board[index];
-    };
-
-   const updateBoard = (playerToken) => {
+   const updateBoard = () => {
        for(let box of boxList) {
             box.addEventListener('click', () => {
+                if(box.innerHTML === 'O' || box.innerHTML === 'X') {
+                    return;
+                };
             board.splice(box.id-1, 1, gameControl.getCurrentSign());
             box.innerHTML = board[box.id-1];
             gameControl.roundCount();
+            gameControl.currentPlayerDisplay();
+            gameControl.checkWins(board);
            });
        };
    };
 
-   return { board, createBoard, updateBoard };
+   const reset = () => {
+    board = ["","","","","","","","",""];
+        for (let box of boxList) {
+            box.innerHTML = '';
+        };
+   };
+
+   return { createBoard, updateBoard, reset };
 })();
 
 const gameControl = (() => {
@@ -51,18 +53,49 @@ const gameControl = (() => {
     let playerX = Player(player1Name, 'X');
     let playerO = Player(player2Name, 'O');
 
+    const currentPlayerDisplay = () => {
+        const sign = document.querySelector('.currentPlayerDisplay');
+        if (!gameOver) {
+            sign.textContent = `Player ${getCurrentSign()} to go`;
+        } else if (gameOver) {
+            sign.textContent = 'Game over!';
+        };
+    };
+
     const getCurrentSign = () => {
         return roundNum % 2 === 1 ? playerO.getToken() : playerX.getToken();
     };
-
-    const endGame = () => {}
 
     const roundCount = () => {
         if(roundNum < 9) {
             roundNum++;
         } else {
-            console.log('Game Over')
-        }
+            gameOver = true;
+            console.log('Game Over');
+        };
+    };
+
+    const endGame = (winner) => {
+        gameOver = true;
+        const boxes = document.querySelectorAll('.gameBox');
+        for (let box of boxes) {
+            box.classList.add('disabled');
+        };
+        const sayResult = document.querySelector('.result');
+        sayResult.textContent = `Player ${winner} is the champion!`;
+    };
+
+    const checkWins = (board) => {
+        winningArray.forEach(function(array) {
+            let a = array[0] - 1, b = array[1] - 1, c = array [2] - 1;
+            if(board[a] === 'X' && board[b] === 'X' && board[c] === 'X'){
+                console.log('X is the winner');
+                endGame('X');
+            } else if (board[a] === 'O' && board[b] === 'O' && board[c] === 'O'){
+                console.log('O is the winner');
+                endGame('O');
+            };
+        });
     };
 
     const winningArray = [
@@ -75,5 +108,15 @@ const gameControl = (() => {
         [1, 5, 9],
         [3, 5, 7]
     ];
-    return { playerX, playerO, getCurrentSign, roundCount }
+
+    const restart = () => {
+        const restartBtn = document.querySelector('#restart');
+        restartBtn.addEventListener('click', () => {
+            roundNum = 1;
+            gameOver = false;
+            gameBoard.reset();
+        });
+    };
+    
+    return { currentPlayerDisplay, getCurrentSign, roundCount, checkWins, restart }
 })();
